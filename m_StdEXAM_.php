7201792df38cdc8b82858db81821a40770b2cc17
Andres <softwareapgm@gmail.com>
2019-04-23 00:55:09 -0400

    Update m_StdEXAM_.php

--- m_StdEXAM_.php
+++ m_StdEXAM_.php
@@ -144,12 +144,11 @@ function ExePython($PythonFile, $_Info, $questionIDX, $newQnames)
 	//And run each indivual TestCase related to question.
 	for($c=0; $c< count($_Info["TCipQ"][$questionIDX]); $c++)
 	{
-		if(py_file_setup(	$filePath,
-											$_Info["StudAnswers"][$questionIDX],
-											$newQnames,
-											$_Info["TCipQ"][$questionIDX][$c],
-											$_Info["CfpQ"][$questionIDX]
-										))
+	 	if(py_file_setup($filePath, 
+				 $_Info["StudAnswers"][$questionIDX],
+				 $newQnames,
+				 $_Info["TCipQ"][$questionIDX][$c], 
+				 $_Info["CfpQ"][$questionIDX]))
 		{
 			$command = "python ".$filePath;
 			//Store Executed Python Code
@@ -273,13 +272,13 @@ function Grading($indata, $_Info)
 		$grades = [ $criteria_point, $criteria_point, ....];
 	*/
 	$criteria_point = array("fnamePoints"=>0,
-							"fname"=>"",
-							"Syntax" => 0, //Colon Mark
-							"TestCases" => array(),
-							"Constraints" => array(),
-							"Comments" => array(),
-							"StudOutput" => array(),
-							"PointsLost" => 0);
+				"fname"=>"",
+				"Syntax" => 0, //Colon Mark
+				"TestCases" => array(),
+				"Constraints" => array(),
+				"Comments" => array(),
+				"StudOutput" => array(),
+				"PointsLost" => 0);
 
 	$grades = array();
 
@@ -292,106 +291,106 @@ function Grading($indata, $_Info)
 		//THE STUDENT ANSWER IS NOT EMPTY.
 		if($_Info["StudAnswers"][$idx] !== '')
 		{
-				// --- GRADE FUNCTION NAME ----
-				if(!FunName($_Info["qname"][$idx], $_Info["StudAnswers"][$idx]))
-				{
-					//BAD FUNCTION NAME
-					$criteria_point["fnamePoints"] = $_Info["PpQ"][$idx]; // POINTS DEDUCTED
-					array_push($qname,newFunName($_Info["StudAnswers"][$idx])); // NEW FUNCTION NAME
-					array_push($criteria_point["Comments"],"- FAIL, function name"); //COMMENT
-					$criteria_point["fname"] = $qname[$idx]; //STORING FUNCTION NAME
-				}
-				else{
-					//GOOD FUNCTION NAME
-					$criteria_point["fnamePoints"] = 0; // POINTS DEDUCTED
-					array_push($qname,$_Info["qname"][$idx]); // FUNCTION NAME
-					array_push($criteria_point["Comments"],"- PASS, function name"); //COMMENT
-					$criteria_point["fname"] = $qname[$idx]; //STORING FUNCTION NAME
-				}
+			// --- GRADE FUNCTION NAME ----
+			if(!FunName($_Info["qname"][$idx], $_Info["StudAnswers"][$idx]))
+			{
+				//BAD FUNCTION NAME
+				$criteria_point["fnamePoints"] = $_Info["PpQ"][$idx]; // POINTS DEDUCTED
+				array_push($qname,newFunName($_Info["StudAnswers"][$idx])); // NEW FUNCTION NAME
+				array_push($criteria_point["Comments"],"- FAIL, function name"); //COMMENT
+				$criteria_point["fname"] = $qname[$idx]; //STORING FUNCTION NAME
+			}
+			else{
+				//GOOD FUNCTION NAME
+				$criteria_point["fnamePoints"] = 0; // POINTS DEDUCTED
+				array_push($qname,$_Info["qname"][$idx]); // FUNCTION NAME
+				array_push($criteria_point["Comments"],"- PASS, function name"); //COMMENT
+				$criteria_point["fname"] = $qname[$idx]; //STORING FUNCTION NAME
+			}
 
-				// --- COLON MARK ----
-				if(colonPFL($_Info["StudAnswers"][$idx]))
+			// --- COLON MARK ----
+			if(colonPFL($_Info["StudAnswers"][$idx]))
+			{
+				//GOOD, : is present.
+				$criteria_point["Syntax"] = 0; // POINTS DEDUCTED
+				array_push($criteria_point["Comments"],"- PASS, Colon present in first line"); //COMMENT
+			}
+			else
+			{
+				//BAD, : is not present.
+				$criteria_point["Syntax"]= $_Info["PpQ"][$idx];// POINTS DEDUCTED
+				array_push($criteria_point["Comments"],"- FAIL, Colon is not present in first line"); //COMMENT
+				//FIX Student Code (add colon to first line)
+				$_Info["StudAnswers"][$idx] = addColonFL($_Info["StudAnswers"][$idx]);
+			}
+
+
+			// --- GRADE CONSTRAINTS ----
+			foreach(qConstraintsGrd($_Info["CfpQ"][$idx], $_Info["StudAnswers"][$idx], $_Info["PpQ"][$idx]) as $Cpoint)
+			{
+				array_push($criteria_point["Constraints"],$Cpoint);
+			}
+
+			//ADDING COMMENTS TO CONSTRAINTS
+			commentsConstraints($criteria_point["Comments"], $criteria_point["Constraints"], $_Info["CfpQ"][$idx]);
+
+			// --- CODE EXECUTION (TEST CASES)----
+			$std_tmp_code_result = ExePython("./02_evaluate/written.py", $_Info, $idx, $qname[$idx]);
+
+			//Compare Std_results with TCoutput
+			for($a =0; $a < count($_Info["TCopQ"][$idx]); $a++)
+			{
+				//Remove "\n" created when Python prints.
+				$std_tmp_code_result[$a] = preg_split('/\n/',$std_tmp_code_result[$a])[0];
+
+				//str_replace use becuase STRINGS and CHARS will come from DB with quotes in it.
+				if($std_tmp_code_result[$a] == str_replace('"','',$_Info["TCopQ"][$idx][$a]))
 				{
-					//GOOD, : is present.
-					$criteria_point["Syntax"] = 0; // POINTS DEDUCTED
-					array_push($criteria_point["Comments"],"- PASS, Colon present in first line"); //COMMENT
+					$tmp_comment = "- PASS, Result: ".$std_tmp_code_result[$a]." | Expected: ".str_replace('"','',$_Info["TCopQ"][$idx][$a]);
+					array_push($criteria_point["StudOutput"],$std_tmp_code_result[$a]); //STUD Output
+					array_push($criteria_point["TestCases"],0); //POINTS
+					array_push($criteria_point["Comments"],$tmp_comment); //COMMENT
+					//echo $tmp_comment."\n";
 				}
 				else
 				{
-					//BAD, : is not present.
-					$criteria_point["Syntax"]= $_Info["PpQ"][$idx];// POINTS DEDUCTED
-					array_push($criteria_point["Comments"],"- FAIL, Colon is not present in first line"); //COMMENT
-					//FIX Student Code (add colon to first line)
-					$_Info["StudAnswers"][$idx] = addColonFL($_Info["StudAnswers"][$idx]);
+					$tmp_comment = "- FAIL, Result: ".$std_tmp_code_result[$a]." | Expected: ".str_replace('"','',$_Info["TCopQ"][$idx][$a]);
+					array_push($criteria_point["StudOutput"],$std_tmp_code_result[$a]); //STUD Output
+					array_push($criteria_point["TestCases"],$_Info["PpQ"][$idx]); //POINTS
+					array_push($criteria_point["Comments"],$tmp_comment); //COMMENT
 				}
-
-
-				// --- GRADE CONSTRAINTS ----
-				foreach(qConstraintsGrd($_Info["CfpQ"][$idx], $_Info["StudAnswers"][$idx], $_Info["PpQ"][$idx]) as $Cpoint)
-				{
-					array_push($criteria_point["Constraints"],$Cpoint);
-				}
-
-				//ADDING COMMENTS TO CONSTRAINTS
-				commentsConstraints($criteria_point["Comments"], $criteria_point["Constraints"], $_Info["CfpQ"][$idx]);
-
-				// --- CODE EXECUTION (TEST CASES)----
-				$std_tmp_code_result = ExePython("./02_evaluate/written.py", $_Info, $idx, $qname[$idx]);
-
-				//Compare Std_results with TCoutput
-				for($a =0; $a < count($_Info["TCopQ"][$idx]); $a++)
-				{
-					//Remove "\n" created when Python prints.
-					$std_tmp_code_result[$a] = preg_split('/\n/',$std_tmp_code_result[$a])[0];
-
-					//str_replace use becuase STRINGS and CHARS will come from DB with quotes in it.
-					if($std_tmp_code_result[$a] == str_replace('"','',$_Info["TCopQ"][$idx][$a]))
-					{
-						$tmp_comment = "- PASS, Result: ".$std_tmp_code_result[$a]." | Expected: ".str_replace('"','',$_Info["TCopQ"][$idx][$a]);
-						array_push($criteria_point["StudOutput"],$std_tmp_code_result[$a]); //STUD Output
-						array_push($criteria_point["TestCases"],0); //POINTS
-						array_push($criteria_point["Comments"],$tmp_comment); //COMMENT
-						//echo $tmp_comment."\n";
-					}
-					else
-					{
-						$tmp_comment = "- FAIL, Result: ".$std_tmp_code_result[$a]." | Expected: ".str_replace('"','',$_Info["TCopQ"][$idx][$a]);
-						array_push($criteria_point["StudOutput"],$std_tmp_code_result[$a]); //STUD Output
-						array_push($criteria_point["TestCases"],$_Info["PpQ"][$idx]); //POINTS
-						array_push($criteria_point["Comments"],$tmp_comment); //COMMENT
-					}
-				}
+			}
 
 		}
 
 			//THE STUDENT ANSWER IS EMPTY
 		else
 		{
-					//Function Name Points
-					$criteria_point["fnamePoints"] = $_Info["PpQ"][$idx]; // POINTS
-					array_push($criteria_point["Comments"],"- FAIL, no function name given");// COMMENT
-					//Function Name
-					array_push($qname," "); // FUNCTION NAME
-					//Colon Mark Points
-					$criteria_point["Syntax"] = $_Info["PpQ"][$idx];
-					array_push($criteria_point["Comments"],"- FAIL, no input given");// COMMENT
-					//Constraints
-					foreach(qConstraintsGrd($_Info["CfpQ"][$idx], $_Info["StudAnswers"][$idx], $_Info["PpQ"][$idx]) as $Cpoint)
-					{
-						array_push($criteria_point["Constraints"],$Cpoint);
-					}
+			//Function Name Points
+			$criteria_point["fnamePoints"] = $_Info["PpQ"][$idx]; // POINTS
+			array_push($criteria_point["Comments"],"- FAIL, no function name given");// COMMENT
+			//Function Name
+			array_push($qname," "); // FUNCTION NAME
+			//Colon Mark Points
+			$criteria_point["Syntax"] = $_Info["PpQ"][$idx];
+			array_push($criteria_point["Comments"],"- FAIL, no input given");// COMMENT
+			//Constraints
+			foreach(qConstraintsGrd($_Info["CfpQ"][$idx], $_Info["StudAnswers"][$idx], $_Info["PpQ"][$idx]) as $Cpoint)
+			{
+				array_push($criteria_point["Constraints"],$Cpoint);
+			}
 
-					array_push($criteria_point["Comments"],"- FAIL, no CONSTRAINTS input given");// COMMENT
+			array_push($criteria_point["Comments"],"- FAIL, no CONSTRAINTS input given");// COMMENT
 
-					//Code Execution (TEST CASES)
-					//NUMBER OF TESTCASES OUPUT
-					for($a =0; $a < count($_Info["TCopQ"][$idx]); $a++)
-					{
-						$tmp_comment = "- FAIL, no input given";
-						array_push($criteria_point["StudOutput"]," "); //STUD Output
-						array_push($criteria_point["TestCases"],$_Info["PpQ"][$idx]); //POINTS
-						array_push($criteria_point["Comments"],$tmp_comment); //COMMENT
-					}
+			//Code Execution (TEST CASES)
+			//NUMBER OF TESTCASES OUPUT
+			for($a =0; $a < count($_Info["TCopQ"][$idx]); $a++)
+			{
+				$tmp_comment = "- FAIL, no input given";
+				array_push($criteria_point["StudOutput"]," "); //STUD Output
+				array_push($criteria_point["TestCases"],$_Info["PpQ"][$idx]); //POINTS
+				array_push($criteria_point["Comments"],$tmp_comment); //COMMENT
+			}
 		}
 
 		// --- Calculate PointsLost ---
@@ -415,13 +414,13 @@ function Grading($indata, $_Info)
 
 		// --- Clear Criteria ---
 		$criteria_point = array("fnamePoints"=>0,
-								"fname"=>"",
-								"Syntax" => 0, //Colon Mark
-								"TestCases" => array(),
-								"Constraints" => array(),
-								"Comments" => array(),
-								"StudOutput" => array(),
-								"PointsLost" => 0);
+					"fname"=>"",
+					"Syntax" => 0, //Colon Mark
+					"TestCases" => array(),
+					"Constraints" => array(),
+					"Comments" => array(),
+					"StudOutput" => array(),
+					"PointsLost" => 0);
 
 	}
 
@@ -466,16 +465,13 @@ $ans = array();
 array_push($ans,$data);
 array_push($ans,$result);
 
-//$t =  json_encode($ans);
-//echo $t;
-
 // --- JSON OBJ ---
 
 
 /*URL TO BACK SERVER*/
 $url = "https://web.njit.edu/~pio3/CS490/exam/submitexam.php";
-//$url = "https://web.njit.edu/~jp594/group5/middle/pwill/submitexam.php";
 
+//cURL to Back Server
 echo http_post_back_server($url, json_encode($ans));
 
 ?>
